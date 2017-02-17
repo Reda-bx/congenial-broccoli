bf2d13c3523fb33a47a18749ab401fc34d2f7542
akram <akram@hiddenfounders.com>
2017-02-17 11:10:34 +0000

    remove unused actions

--- src/overlay.js
+++ src/overlay.js
@@ -1,55 +1,35 @@
-import TrelloService from './TrelloService'
-import db from '../db'
-import {populateSelectBox, extractEmailFromDescription} from './Utils'
 import map from 'lodash/map'
+import axios from 'axios'
 
 let globalList;
-let globalCard;
-
-// init trello api
-Trello.setKey(db.APP_KEY) // TODO: move to specific constants file
-Trello.setToken(localStorage.getItem('trello_token') || db.TRELLO_TOKEN)
 
 // get trello power up iframe
 const t = TrelloPowerUp.iframe()
 
-// instantiate and init trello service
-const trelloService = new TrelloService()
-trelloService.init(Trello)
-
 // it all starts here
-trelloService.getCurrentList(t)
-.then(list => {
-  console.log('list details: ')
-  console.log(list)
-  // make list details global
-  globalList = list
-  return trelloService.getCurrentCard(t)
+// get current list
+t.list('id')
+.then(result => {
+  const {id} = result
+  return axios.get(`https://hf-automator.herokuapp.com/lists/${id}`)
 })
-.then(card => {
-  console.log('card details: ')
-  console.log(card)
-  // make card details global
-  globalCard = card
-  return true
-})
-.then(params => {
+.then(res => {
+  const {data} = res
+  globalList = data
   // populate select box with subjects
   const subjectsDOM = document.getElementById('subjects')
-  const options = map(globalList.emails, 'subject')
+  const options = map(data.checklist.items, 'subject')
   populateSelectBox(subjectsDOM,options)
-  // create subjects check list
-  trelloService.createCheckList(globalCard.id, 'Follow Ups', options)
-  return trelloService.getCardDescription(t)
+  return t.card('desc')
 })
 .then(result => {
   // extract email from description and attach it to input
   const email = extractEmailFromDescription(result.desc)
   if(email) document.getElementById('to').value = email
-  return true
-})
-.then(result => {
   console.log('done')
+})
+.catch(err => {
+  console.log(err)
 })
 
 t.render(() => {});
@@ -59,7 +39,7 @@ document.getElementById('subjects').onchange = function(e) {
   // can't arrow this because of the lexical 'this'
   const value = this[this.selectedIndex].value
   // get subject and value from email
-  const email = globalList.emails.find(email => email.subject === value)
+  const email = globalList.checklist.items.find(item => item.subject === value)
   // set subject and body to email
   document.getElementById('subject').value = email.subject;
   document.getElementById('body').value = email.body;
@@ -67,42 +47,12 @@ document.getElementById('subjects').onchange = function(e) {
 
 // send email event handler
 document.getElementById('send').addEventListener('click', () => {
-// get input values
-const subject = document.getElementById('subject').value
-const body = document.getElementById('body').value
-const to = document.getElementById('to').value
-const cc = document.getElementById('cc').value
-if(globalList.name === "Typeform Application"){
-  // get call list id
-  const callListID = db.lists.find(list => list.name === "Call").id
-  const {id} = globalCard
-  Promise.all([
-    // move to call list
-    trelloService.moveCard(id,callListID),
-    // assign to akram
-    trelloService.assignMember(id,db.members.akram),
-    // add his move label
-    trelloService.addLabel(id,db.labels.his_move),
-    // add two days to due date
-    trelloService.setDueDate(id, 2)
-  ])
-  .then(result => console.log('chain done'))
-}
-console.log(`to: ${to}, cc: ${cc}, subject: ${subject}, body: ${body}`)
-trelloService.getCardCheckLists(globalCard.id)
-.then(checkLists => {
-  console.log('checkLists: ')
-  console.log(checkLists)
-  // get subjects checklist
-  const checkList = checkLists.find(checkList => checkList.name === 'Follow Ups')
-  // get subject of sent email
-  const checkItem = checkList.checkItems.find(checkItem => checkItem.name === subject)
-  // mark the subject as checked in the checklist
-  return trelloService.setCheckListItem(globalCard.id, checkList.id, checkItem.id, true)
-})
-.then(result => {
-  t.closeOverlay().done()
-})
+  // get input values
+  const subject = document.getElementById('subject').value
+  const body = document.getElementById('body').value
+  const to = document.getElementById('to').value
+  const cc = document.getElementById('cc').value
+  // t.closeOverlay().done()
 })
 
 // close overlay if user presses escape key
@@ -124,6 +74,20 @@ document.getElementById('close').addEventListener('click', () => {
     t.closeOverlay().done()
 })
 
-// TODO: Backlog:
+// TODO: Backlog
 // - markdown/format email
 // - eslint
+
+function populateSelectBox(select, options){
+  options.forEach(option => {
+    const domOption = document.createElement('option')
+    domOption.value = option
+    domOption.innerHTML = option
+    select.appendChild(domOption)
+  })
+}
+
+function extractEmailFromDescription(description){
+  const emails = description.match(/([a-zA-Z0-9._-]+@[a-zA-Z0-9._-]+\.[a-zA-Z0-9._-]+)/gi)
+  return (emails.length > 0) ? emails[0] : null
+}
